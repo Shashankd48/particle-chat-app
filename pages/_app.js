2ca1163b9ae4d3c50a0635b96d432f42fd3336d8
Shashank Dubey <jfriday464@gmail.com>
2021-09-10 19:08:08 +0530

    Working with firestore

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,19 @@
 import "../styles/globals.css";
 import { useAuthState } from "react-firebase-hooks/auth";
-import { auth } from "../firebase";
+import { auth, db } from "../firebase";
 import Login from "./login";
 import Loading from "../components/Loading";
+import { useEffect } from "react";
+import { collection } from "firebase/firestore";
 
 function MyApp({ Component, pageProps }) {
    const [user, loading] = useAuthState(auth);
+
+   useEffect(() => {
+      if (user) {
+         //  collection(db, "users");
+      }
+   }, [user]);
 
    if (loading) return <Loading />;
 
