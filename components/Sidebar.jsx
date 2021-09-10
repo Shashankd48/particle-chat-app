3c7fba186a399235f3e4d5e63014c2187b0163dc
Shashank Dubey <jfriday464@gmail.com>
2021-09-10 17:15:30 +0530

    Working with styled components

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -1,14 +1,31 @@
 import styled from "styled-components";
+import { Avatar, IconButton } from "@material-ui/core";
+import {
+   Chat as ChatIcon,
+   MoreVert as MoreVertIcon,
+   Search as SearchIcon,
+} from "@material-ui/icons";
 
 const Sidebar = () => {
    return (
       <Container>
-         <Header>header</Header>
-         <Paragraph>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia
-            voluptatibus nobis in reiciendis voluptate deleniti vero. Eum
-            tenetur maiores earum.{" "}
-         </Paragraph>
+         <Header>
+            <UserAvatar />
+
+            <IconsContainer>
+               <IconButton>
+                  <ChatIcon />
+               </IconButton>
+
+               <IconButton>
+                  <MoreVertIcon />
+               </IconButton>
+            </IconsContainer>
+         </Header>
+
+         <Search>
+            <SearchIcon />
+         </Search>
       </Container>
    );
 };
@@ -17,7 +34,29 @@ export default Sidebar;
 
 const Container = styled.div``;
 
-const Header = styled.div``;
+const Header = styled.div`
+   display: flex;
+   position: sticky;
+   top: 0;
+   background-color: #fff;
+   z-index: 1;
+   justify-content: space-between;
+   align-items: center;
+   padding: 15px;
+   height: 80px;
+   border-bottom: 3px solid whitesmoke;
+`;
+
+const UserAvatar = styled(Avatar)`
+   cursor: pointer;
+   :hover {
+      opacity: 0.8;
+   }
+`;
+
+const Search = styled.div``;
+
+const IconsContainer = styled.div``;
 
 const Paragraph = styled.p`
    font-size: 20px;
