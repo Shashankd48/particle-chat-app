d42754399cc4848f03f1d29012876cee92d8e544
Shashank Dubey <jfriday464@gmail.com>
2021-09-23 15:55:19 +0530

    Meta information added

--- components/Titlebar.jsx
+++ components/Titlebar.jsx
@@ -1,15 +1,24 @@
 import Head from "next/dist/shared/lib/head";
 
-const WhatsappIcon = "/favicon.svg";
-
-const Titlebar = ({ title, favicon = WhatsappIcon }) => {
+const Titlebar = ({ title, favicon = "/favicon.svg" }) => {
    return (
       <Head>
          <title>{title}</title>
          <meta
             name="description"
-            content="Particle-Chat is a free app that works with you email."
+            content="Particle-Chat is a free chat app that works with you gmail account."
          />
+         <meta
+            name="keywords"
+            content="Chat-APP, Free-chat-app, app, chat, nextjs-app, nextjs, react"
+         />
+         <meta name="author" content="Xparticle.in" />
+         <meta
+            name="viewport"
+            content="width=device-width, initial-scale=1.0"
+         />
+
+         <meta name="application-name" content="Particle-Chat" />
          <link rel="icon" href={favicon} />
       </Head>
    );
