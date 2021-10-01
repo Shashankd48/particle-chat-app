5a937b93c80d5ae0eded4b3f097ae759941b6742
Shashank Dubey <jfriday464@gmail.com>
2021-10-01 08:08:59 +0530

    NProgress added

--- pages/_app.js
+++ pages/_app.js
@@ -5,9 +5,23 @@ import Login from "./login";
 import Loading from "../components/Loading";
 import { useEffect } from "react";
 import { setDoc, Timestamp, doc } from "firebase/firestore";
+import NProgress from "nprogress";
+import Router from "next/router";
+import "nprogress/nprogress.css";
+
+NProgress.configure({
+   minimum: 0.3,
+   easing: "ease",
+   speed: 800,
+   showSpinner: false,
+});
 
 function MyApp({ Component, pageProps }) {
    const [user, loading] = useAuthState(auth);
+
+   Router.events.on("routeChangeStart", () => NProgress.start());
+   Router.events.on("routeChangeComplete", () => NProgress.done());
+   Router.events.on("routeChangeError", () => NProgress.done());
 
    const setUser = async (user) => {
       try {
